fce2f556eed6a69502e41ce363e019afb672a23d
andy8421 <yichang8421@qq.com>
2021-08-26 00:15:23 +0800

    remove unused files

---
+++ src/components/Button.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+            color: red;
+            border: none;
+            background: green;
+            &:hover{
+              background: #000;
+            }
+            `;
+
+export default Button;
