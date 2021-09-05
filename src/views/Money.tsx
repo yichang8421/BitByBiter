5de08b82869c03cf0e5c9c3faba4ae98a889f22e
andy8421 <yichang8421@qq.com>
2021-09-05 15:40:00 +0800

    完成 RecordSelection.tsx 基本 UI

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,7 +1,50 @@
 import Layout from "../components/Layout";
-import React from "react";
+import React, {useCallback, useState} from "react";
 import styled from "styled-components";
 import Icon from "components/Icon";
+
+const RecordSelection = styled.section`
+    border:1px solid red;
+    padding: 22px 6px;
+    padding-left: 26px;
+    font-size: 14px;
+    display: flex;
+    justify-content: space-between;
+    > label{
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        justify-content: left;
+        > span{
+            //margin-right: 8px;
+            white-space: nowrap;
+            font-weight: bolder;
+        }
+        > select{
+            display: block;
+            padding: 6px 2px;
+                        border:1px solid red;
+            font-weight: bolder;
+            background: yellowgreen;
+            //border:none;
+            border-radius: 20px;
+        }
+    }
+    > .output{
+        display: block;
+        width: 100%;
+        background: white;
+        margin-left: 16px;
+        margin-right: 16px;
+        font-size: 28px;
+        font-family: Consolas monospace;
+        padding: 9px 8px;
+        text-align: right;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        overflow: hidden;    
+    }
+`;
 
 const NotesSection = styled.section`
     background: #f5f5f5;
@@ -18,7 +61,7 @@ const NotesSection = styled.section`
         > input{
             display: block;
             width: 100%;
-            height: 72px;
+            height: 48px;
             background: none;
             border:none;
         }
@@ -27,7 +70,6 @@ const NotesSection = styled.section`
 
 const TagsSelection = styled.section`
     background: #fff;
-    border:1px solid red;
     overflow-x: hidden;
     > ol{
         margin:0 -12px;
@@ -35,10 +77,10 @@ const TagsSelection = styled.section`
         flex-wrap: wrap;
         >li{
           &.selected{
-            background: #ff6200;
-            border:1px solid red;
-            box-shadow: inset 0 0 5px rgb(0 0 0 / 15%);
-            border-radius: 20px;
+            //background: #ff6200;
+            //border:1px solid red;
+            //box-shadow: inset 0 0 5px rgb(0 0 0 / 15%);
+            //border-radius: 20px;
           }
             
             text-align: center;
@@ -78,13 +120,41 @@ const TagsSelection = styled.section`
 
 const NumperPadSelection = styled.section``;
 
+export const BILL_TYPE = {
+    PAYMENT: "payment",
+    INCOME: "income",
+};
+
 function Money() {
+    // const [showTypeOptions, setShowTypeOptions] = useState(false);
+    // const onToggle = useCallback(() => {
+    //     setShowTypeOptions(!showTypeOptions);
+    // }, [showTypeOptions]);
+
     return (
         <Layout>
+            <RecordSelection>
+                <label>
+                    <span>
+                        收支类型
+                    </span>
+                    <select
+                        name="types"
+                        id="types"
+                        // onChange={onToggle}
+                    >
+                        <option value="1">收入</option>
+                        <option value="2">支出</option>
+                    </select>
+                </label>
+                <div className="output">
+                    hello
+                </div>
+            </RecordSelection>
             <NotesSection>
                 <label>
                     <span>备注</span>
-                    <input type="text"  placeholder="此处添加备注"/>
+                    <input type="text" placeholder="此处添加备注"/>
                 </label>
             </NotesSection>
             <TagsSelection>
