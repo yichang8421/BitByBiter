e46bd75f342b959b6eca4ded89b3f68b460e5f1a
andy8421 <yichang8421@qq.com>
2021-09-12 23:48:49 +0800

    优化 Icon.tsx

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import classnames from "classnames";
 
 let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
 try {importAll(require.context("icons", true, /\.svg$/));} catch (error) {console.log(error);}
@@ -6,11 +7,12 @@ try {importAll(require.context("icons", true, /\.svg$/));} catch (error) {consol
 type Props = {
     name?: string,
     fill?: string
-}
+} & React.SVGProps<SVGSVGElement>;
 
-const Icon = (props: Props) => {
+const Icon: React.FC<Props> = (props: Props) => {
+    const {name, fill, className, children, ...rest} = props;
     return (
-        <svg className="icon" fill={props.fill}>
+        <svg className={classnames("icon", className)} {...rest}>
             {props.name && <use xlinkHref={"#" + props.name}/>}
         </svg>
     );
