7c239145a385bfcc73ecbef929be39571522df40
andy8421 <yichang8421@qq.com>
2021-09-16 23:48:48 +0800

    抽离 TypeSelection.tsx

--- src/views/Ledger.tsx
+++ src/views/Ledger.tsx
@@ -5,37 +5,8 @@ import {useRecords} from "../hooks/useRecords";
 import {useTags} from "../hooks/useTags";
 import {ElementCenter} from "../components/CalculatorOutput/ElementCenter";
 import {Space} from "../components/CalculatorOutput/Space";
+import {TypeSelection} from "../components/TypeSelection";
 import dayjs from "dayjs";
-
-const Wrapper = styled.section`
-    font-size: 20px;
-    font-weight: bolder;
-    >ul{
-        display: flex;
-        >li{
-            width: 50%;
-            text-align: center;
-            padding: 16px 0;
-            position:relative;
-            &.selected{
-                background: #e7f2ff;
-                border-top-left-radius: 10px;
-                border-top-right-radius: 10px;
-                box-shadow: inset 0 0 2px rgba(0, 0, 0, 0.05);
-                &::after{
-                    content: "";
-                    display: block;
-                    height: 3px;
-                    background: #9acd32;
-                    position:absolute;
-                    bottom: 0;
-                    width: 100%;
-                    left: 0;
-                }
-            }
-        }
-    }
-`;
 
 const Item = styled.div`
     display: flex;
@@ -64,36 +35,6 @@ const Header = styled.h3`
     display: flex;
     justify-content: space-between;
 `;
-
-type Props = {
-    value: RecordType;
-    onChange: (value: RecordType) => void;
-}
-
-const TypeSelection: React.FC<Props> = (props: Props) => {
-    const typeMap = {
-        "-": "支出",
-        "+": "收入"
-    };
-    const [typeList] = useState<RecordType[]>(["-", "+"]);
-    const type = props.value;
-
-    return (
-        <Wrapper>
-            <ul>{
-                typeList.map(c =>
-                    <li
-                        key={c}
-                        className={type === c ? "selected" : ""}
-                        onClick={() => {
-                            props.onChange(c);
-                        }}
-                    >{typeMap[c]}</li>
-                )
-            }</ul>
-        </Wrapper>
-    );
-};
 
 function Ledger() {
     const [type, setType] = useState<RecordType>("-");
